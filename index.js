3711a35eacf926e1dace9d3a9e532045388e2e01
anastasia <anastasia.santoso.id@gmail.com>
2018-09-10 17:58:18 +0700

    Release 1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class Animal {
   constructor(legs, blood) {
     this._legs = legs || 2;
     this._warm_blooded = blood || true;
+    this._super_power = new SuperPower();
   }
 }
 
@@ -23,13 +24,25 @@ class Horse extends Animal {
   constructor() {
     super(4, true);
   }
+}
 
+class SuperPower {
+  constructor () {}
+  use_laser_vision() {
+    console.log('For Asgard!');
+  }
+  be_invisible() {
+    console.log('Leave Hulk alone!');
+  }
 }
 
 let animal = new Animal();
-let dog = new Dog();
+let dog = new Dog(4, true);
 let cat = new Cat();
 let panda = new Panda()
 let horse = new Horse();
 console.log(animal._legs); // 2
+console.log(dog._legs); // 4
 console.log(horse._legs); // 4
+dog._super_power.use_laser_vision(); // For Asgard!
+cat._super_power.be_invisible(); //Leave Hulk alone!
